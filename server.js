cf63dade54a04b749aa4c0fb3a99f62e644484e2
stojanvan <stojan.vancouver@gmail.com>
2016-06-13 13:59:27 -0700

    'committing'

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end('<h1>Hello</h1');
+});
+
+var port = process.env.PORT || 3000;
+
+server.listen(port);
+
+
