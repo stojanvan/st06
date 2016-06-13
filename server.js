519a955a2d314a1c48721d5df8d88f6dece1507e
stojanvan <stojan.vancouver@gmail.com>
2016-06-13 14:30:26 -0700

    'server.js_modified'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'content-type': 'text/html'});
-    res.end('<h1>Hello</h1');
+    res.end('<h1>Hello st06</h1');
 });
 
 var port = process.env.PORT || 3000;
